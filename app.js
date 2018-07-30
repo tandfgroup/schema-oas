54988b8aa5efa6a9e9df8373188669ab295b6e77
Sean Trane <seantrane@users.noreply.github.com>
2018-07-30 00:13:03 -0400

    build: add files to serve swagger-ui via nodejs/express

---
+++ app.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
+
+const app = express();
+
+const port = 3000;
+
+app.locals.name = require('./package.json').name || 'app';
+app.locals.version = require('./package.json').version || 'latest';
+app.locals.tag = `${app.locals.name}:${app.locals.version}`;
+
+app.use(express.static(pathToSwaggerUi));
+// app.use(express.static(__dirname));
+
+app.listen(port);
+
+console.log(`${app.locals.tag} now running on port `, port);
